00c7dcb2486fb6b2540287027afc8e7770a0991b
p2lifau <p2lifau@gmail.com>
2022-03-20 20:57:54 -0700

    Initial Commit

---
+++ Emoji Dictionary/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Emoji Dictionary
+//
+//  Created by Pierre Tulifau on 3/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Emoji Dictionary/Emoji_DictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_DictionaryApp.swift
+//  Emoji Dictionary
+//
+//  Created by Pierre Tulifau on 3/20/22.
+//
+
+import SwiftUI
+
+@main
+struct Emoji_DictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
